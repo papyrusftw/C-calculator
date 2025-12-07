763e346c02272001131c4ba9e67c1df08733993b
papyrusftw <105867908+papyrusftw@users.noreply.github.com>
2025-12-07 22:09:00 +0000

    Update Calc.C

--- Calc.C
+++ Calc.C
@@ -4,49 +4,56 @@
 #include <stdlib.h>
 
 int isInteger(const char *str) {
-    if (*str == '-') str++;
-    if (*str == '\0') return 0;
+    if (*str == '-') str++;     // allow negative numbers
+    if (*str == '\0') return 0; // reject "-" only
     while (*str) {
         if (!isdigit(*str)) return 0;
         str++;
     }
     return 1;
 }
+
 int main() {
     char operator;
     char input1[50], input2[50];
-    int num1, num2;
+    int int1, int2;
+    double num1, num2;
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
     printf("Enter first integer: ");
     scanf("%s", input1);
+    
     if (!isInteger(input1)) {
         printf("Error: First input is not an integer.\n");
         return 1;
     }
+
     printf("Enter second integer: ");
     scanf("%s", input2);
+
     if (!isInteger(input2)) {
         printf("Error: Second input is not an integer.\n");
         return 1;
     }
     
-    num1 = atoi(input1);
-    num2 = atoi(input2);
-    
+    int1 = atoi(input1);
+    int2 = atoi(input2);
+    num1 = (double)int1;
+    num2 = (double)int2;
+
     switch (operator) {
         case '+':
-            printf("Result: %d\n", num1 + num2);
+            printf("Result: %.2f\n", num1 + num2);
             break;
         case '-':
-            printf("Result: %d\n", num1 - num2);
+            printf("Result: %.2f\n", num1 - num2);
             break;
         case '*':
-            printf("Result: %d\n", num1 * num2);
+            printf("Result: %.2f\n", num1 * num2);
             break;
         case '/':
             if (num2 != 0)
-                printf("Result: %d\n", num1 / num2);
+                printf("Result: %.2f\n", num1 / num2);
             else
                 printf("Error: Division by zero isnt possible.\n");
             break;
